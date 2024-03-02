a0ce60003f9afc7773db1329cceda8261b453ee8
Jainam Patel <jainampatel68@gmail.com>
2024-03-01 22:04:28 -0500

    Made the class suitable for tempreture sensor

--- lab_assignment_6.py
+++ lab_assignment_6.py
@@ -14,39 +14,27 @@ Date: March 3, 2024
 import matplotlib.pyplot as plt
 import random
 
-class DataGenerator:
-    def __init__(self):
+class TemperatureSensor:
+    def __init__(self, min_temp=18, max_temp=23):
+        self.min_temp = min_temp
+        self.max_temp = max_temp
         self.data = []
-        
+
     def _generate_random_value(self):
         return random.uniform(0, 1)
-    
 
     @property
-    def random_value(self):
-        value = self._generate_random_value()
-        if len(self.data) < 2:
-            self.data.append(value)
-        else:
-            m = max(self.data) - min(self.data)
-            c = min(self.data)
-            value = m * value + c
-            self.data.append(value)
-        print(f'Generated value: {value}')
-        
-    
-    def plot_data(self):
-        plt.plot(self.data, 'r-')
-        plt.title('Random Numbers')
-        plt.xlabel('Index')
-        plt.ylabel('Value')
-        plt.show()
-
+    def temperature(self):
+        normalized_value = self._generate_random_value()
+        temperature = self.min_temp + (self.max_temp - self.min_temp) * normalized_value
+        self.data.append(temperature)
+        return temperature
 
 if __name__ == "__main__":
-    rng = DataGenerator()
+    sensor = TemperatureSensor()
     for i in range(10):
-        rng.random_value
-    rng.plot_data()
-
+        print(sensor.temperature)
+    plt.plot(sensor.data)
+    plt.show()
+    print(sensor.data)
 
