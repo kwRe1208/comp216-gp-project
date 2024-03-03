1a95676ef56a99573ed7aeec19e0e4e89c3f3f66
kevinwong1208 <ywong140@my.centennialcollege.ca>
2024-03-03 13:45:11 -0500

    Added docstrings

--- lab_assignment_6.py
+++ lab_assignment_6.py
@@ -14,39 +14,65 @@ Date: March 3, 2024
 import matplotlib.pyplot as plt
 import random
 
-class DataGenerator:
-    def __init__(self):
+class TemperatureSensor:
+    """
+    A class representing a temperature sensor.
+
+    Attributes:
+        min_temp (float): The minimum temperature value.
+        max_temp (float): The maximum temperature value.
+        data (list): A list to store the generated temperature values.
+
+    Methods:
+        _generate_random_value: Generates a random value between 0 and 1.
+        temperature: Generates a random temperature value within the specified range.
+
+    """
+
+    def __init__(self, min_temp=18, max_temp=23):
+        """
+        Initializes a TemperatureSensor object.
+
+        Args:
+            min_temp (float): The minimum temperature value (default: 18).
+            max_temp (float): The maximum temperature value (default: 23).
+
+        """
+        self.min_temp = min_temp
+        self.max_temp = max_temp
         self.data = []
-        
+
     def _generate_random_value(self):
+        """
+        Generates a random value between 0 and 1.
+
+        Returns:
+            float: A random value between 0 and 1.
+
+        """
         return random.uniform(0, 1)
-    
 
     @property
-    def random_value(self):
-        value = self._generate_random_value()
-        if len(self.data) < 2:
-            self.data.append(value)
-        else:
-            m = max(self.data) - min(self.data)
-            c = min(self.data)
-            value = m * value + c
-            self.data.append(value)
-        print(f'Generated value: {value}')
-        
-    
-    def plot_data(self):
-        plt.plot(self.data, 'r-')
-        plt.title('Random Numbers')
-        plt.xlabel('Index')
-        plt.ylabel('Value')
-        plt.show()
+    def temperature(self):
+        """
+        Generates a random temperature value within the specified range.
 
+        Returns:
+            float: A random temperature value within the specified range.
+
+        """
+        normalized_value = self._generate_random_value()
+        temperature = self.min_temp + (self.max_temp - self.min_temp) * normalized_value
+        self.data.append(temperature)
+        return temperature
 
 if __name__ == "__main__":
-    rng = DataGenerator()
+    sensor = TemperatureSensor()
     for i in range(10):
-        rng.random_value
-    rng.plot_data()
-
-
+        print(sensor.temperature)
+    plt.plot(sensor.data)
+    plt.title('Temperature Sensor')
+    plt.xlabel('Time')
+    plt.ylabel('Temperature')
+    plt.show()
+    print(sensor.data)
