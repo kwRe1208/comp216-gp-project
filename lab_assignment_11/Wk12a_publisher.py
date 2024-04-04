937c8beefd4695fad2c44384294c542d6a6c5982
kevinwong1208 <ywong140@my.centennialcollege.ca>
2024-04-03 21:56:39 -0400

    Updated subscriber code to make it works

--- lab_assignment_11/Wk12a_publisher.py
+++ lab_assignment_11/Wk12a_publisher.py
@@ -1,3 +1,17 @@
+"""
+COMP216 - Lab Assignment 11 - Publisher
+
+Group: 1
+Group Members:
+    Handa, Karan
+    Ngan, Tsang Kwong
+    Patel, Jainam
+    Wong, Yu Kwan
+    ZHANG, AILIN
+
+Date: April 6, 2024
+"""
+
 #Import utils.py
 from utils import Util
 import paho.mqtt.client as mqtt
@@ -5,43 +19,103 @@ from random import uniform
 import time
 import json
 
+
 class Publisher(Util):
+    """
+    A class representing a publisher that publishes data to an MQTT server.
+
+    Attributes:
+        broker (str): The MQTT broker address.
+        port (int): The MQTT broker port.
+        callback_api_ver (int): The MQTT callback API version.
+        client_id (str): The client ID for the MQTT client.
+        topic (str): The topic to publish the data to.
+        client_pub (mqtt.Client): The MQTT client instance.
+
+    Methods:
+        convert_to_json: Converts the data obtained from Util to JSON format.
+        create_client: Creates an MQTT client and connects to the broker.
+        on_connect: Callback function called when the client connects to the broker.
+        publish_data: Publishes the data to the MQTT server.
+        on_publish: Callback function called after publishing data.
+        disconnect: Disconnects the MQTT client from the broker.
+    """
+
     def __init__(self):
         super().__init__()
         self.broker = "mqtt.eclipseprojects.io"
         self.port = 1883
         self.callback_api_ver = mqtt.CallbackAPIVersion.VERSION2
-        self.client_id = "COMP-216-2024-GP2"
+        self.client_id = "COMP216-2024-GP1"
+        self.topic = "TEMP-COMP216-GP1"
 
-    #convert the data get from Util to json format
     def convert_to_json(self):
+        """
+        Converts the data obtained from Util to JSON format.
+
+        Returns:
+            str: The JSON representation of the data.
+        """
         data = self.create_data()
         return json.dumps(data)
 
-    #Create a mqtt client
     def create_client(self):
-        self.client_pub = mqtt.Client(self.callback_api_ver, client_id=self.client_id)
-        self.client_pub.connect(self.broker, self.port)
-        self.client_pub.on_connect = self.on_connect
-        print("Client created")
-        self.client_pub.loop_start()
-    
-    #Connect to the server
-    def on_connect(self, client, userdata, flags, reason, properties):
-        print(f"Connected with reason code {reason}")
+        """
+        Creates an MQTT client and connects to the broker.
 
-    #Publish the data to the server
+        Raises:
+            TimeoutError: If the connection to the broker times out.
+        """
+        try:
+            self.client_pub = mqtt.Client(self.callback_api_ver, client_id=self.client_id)
+            self.client_pub.connect(self.broker, self.port)
+            self.client_pub.on_connect = self.on_connect
+            print("Client created")
+            self.client_pub.loop_start()
+        except TimeoutError:
+            raise TimeoutError("Connection timeout")
+
+    def on_connect(self, client, userdata, flags, reason, properties):
+        """
+        Callback function called when the client connects to the broker.
+
+        Args:
+            client (mqtt.Client): The MQTT client instance.
+            userdata: The user data associated with the client.
+            flags: The flags associated with the connection.
+            reason (int): The reason code for the connection.
+            properties: The properties associated with the connection.
+        """
+        print("Connected with result code {0}".format(str(reason)))
+
     def publish_data(self):
+        """
+        Publishes the data to the MQTT server.
+        """
         data = self.convert_to_json()
-        self.client_pub.publish("TEMP-COMP216", data)
-        self.on_publish(self.client_pub, data)
-        
+        try:
+            self.client_pub.publish(self.topic, data)
+            self.on_publish(self.client_pub, data)
+        except Exception as e:
+            raise e
+        finally:
+            self.client_pub.disconnect()
 
     def on_publish(self, client, userdata):
+        """
+        Callback function called after publishing data.
+
+        Args:
+            client (mqtt.Client): The MQTT client instance.
+            userdata: The user data associated with the client.
+        """
         print(client)
         print(userdata)
-    
+
     def disconnect(self):
+        """
+        Disconnects the MQTT client from the broker.
+        """
         self.client_pub.loop_stop()
         self.client_pub.disconnect()
 
@@ -53,4 +127,4 @@ while True:
     pub.publish_data()
     time.sleep(2)
 
-pub.disconnect()
+#pub.disconnect()
--- lab_assignment_11/Wk12a_subscriber.py
+++ lab_assignment_11/Wk12a_subscriber.py
@@ -1,31 +1,110 @@
+"""
+COMP216 - Lab Assignment 11 - Subscriber
+
+Group: 1
+Group Members:
+    Handa, Karan
+    Ngan, Tsang Kwong
+    Patel, Jainam
+    Wong, Yu Kwan
+    ZHANG, AILIN
+
+Date: April 6, 2024
+"""
+
 import json
 import paho.mqtt.client as mqtt
 
 
 class Subscriber:
+    """
+    A class representing an MQTT subscriber.
+
+    Attributes:
+        broker (str): The MQTT broker to connect to.
+        port (int): The port number of the MQTT broker.
+        client_id (str): The client ID for the MQTT connection.
+        callback_api_ver (int): The version of the MQTT callback API to use.
+        topic (str): The topic to subscribe to.
+
+    Methods:
+        create_client: Creates an MQTT client and connects to the broker.
+        on_connect: Callback function called when the client connects to the broker.
+        on_message: Callback function called when a message is received.
+        print_dictionary: Prints the contents of a dictionary.
+        disconnect: Disconnects the MQTT client from the broker.
+    """
+
     def __init__(self):
         self.broker = "mqtt.eclipseprojects.io"
         self.port = 1883
-        self.client_id = "COMP-216-2024-GP2"
+        self.client_id = "COMP216-2024-GP1"
         self.callback_api_ver = mqtt.CallbackAPIVersion.VERSION2
+        self.topic = "TEMP-COMP216-GP1"
 
     def create_client(self):
-        self.client = mqtt.Client(callback_api_version=self.callback_api_ver)
-        self.client.connect(self.broker, self.port)
-        self.client.subscribe(self.client_id)
-        self.client.on_message = self.on_message
-        self.client.loop_forever()
+        """
+        Creates an MQTT client and connects to the broker.
+        """
+        try:
+            self.client = mqtt.Client(callback_api_version=self.callback_api_ver)
+            self.client.connect(self.broker, self.port)
+            self.client.on_connect = self.on_connect
+            self.client.subscribe(self.topic)
+            self.client.on_message = self.on_message
+            self.client.loop_forever()
+        except TimeoutError:
+            print("Connection to the broker timed out")
 
-    def on_message(self, msg):
-        decoded_message = msg.payload.decode('utf-8')
-        message_dict = json.loads(decoded_message)
-        self.print_dictionary(message_dict)
+    def on_connect(self, client, userdata, flags, reason, properties):
+        """
+        Callback function called when the client connects to the broker.
+
+        Args:
+            client (mqtt.Client): The MQTT client instance.
+            userdata: The user data associated with the client.
+            flags: The flags associated with the connection.
+            reason (int): The reason code for the connection.
+            properties: The properties associated with the connection.
+        """
+        print("Connected with result code {0}".format(str(reason)))
+
+    def on_message(self, client, userdata, msg):
+        """
+        Callback function called when a message is received.
+
+        Args:
+            client (mqtt.Client): The MQTT client instance.
+            userdata: The user data associated with the client.
+            msg (mqtt.MQTTMessage): The received message.
+        """
+        try:
+            decoded_message = msg.payload.decode('utf-8')
+            message_dict = json.loads(decoded_message)
+            self.print_dictionary(message_dict)
+        except json.JSONDecodeError:
+            raise ValueError("Invalid JSON format")
+        finally:
+            self.disconnect()
 
     def print_dictionary(self, dictionary):
+        """
+        Prints the contents of a dictionary.
+
+        Args:
+            dictionary (dict): The dictionary to print.
+        """
         print("id:", dictionary['id'])
         print("time:", dictionary['time'])
         print("temp:", dictionary['temp'])
         print("level:", dictionary['level'])
+    
+    def disconnect(self):
+        """
+        Disconnects the MQTT client from the broker.
+        """
+        self.client.loop_stop()
+        self.client.disconnect()
 
 
 sub = Subscriber()
